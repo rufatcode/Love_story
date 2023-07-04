e5178de66ad6b4af00dfc2f79db084611f2a353b
alihasanli <ahasanli639.dev@gmail.com>
2023-07-04 20:32:12 +0400

    change gmail icon and add sign up button on sign in page

--- love_story/love_story/static/js/login.js
+++ love_story/love_story/static/js/login.js
@@ -1,17 +1,16 @@
-let input = document.querySelector("#id_password")
-let hide = document.querySelector(".hide")
-let show = document.querySelector(".show")
-let view = document.querySelector("#pass-view")
+let pass_inp = document.querySelector("#id_password");
+let hide = document.querySelector(".hide");
+let show = document.querySelector(".show");
+let view = document.querySelector(".pass-view");
 
 view.onclick = function() {
-    if (input.type === 'password') {
-        input.setAttribute('type', 'text') 
-        hide.classList.remove("block")
-        show.classList.add("block")  
-    }
-    else {
-        input.setAttribute('type', 'password')
-        hide.classList.add("block")
-        show.classList.remove("block")
+    if (pass_inp.type === 'password') {
+        pass_inp.setAttribute('type', 'text');
+        hide.classList.remove("block");
+        show.classList.add("block");
+    } else {
+        pass_inp.setAttribute('type', 'password');
+        hide.classList.add("block");
+        show.classList.remove("block");
     }
 }
